ed6120b9aaf8d0e7c20b234b1cbc99f5ad7851eb
jmk255 <jmk255@naver.com>
2024-11-19 17:13:04 +0900

    1119-2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/videos/VideoSlider'
+
+//필요한 데이터를 임포트
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
 
 const Home = () => {
   return (
@@ -14,10 +17,11 @@ const Home = () => {
       description="제 유튜버 사이트에 오신 것을 환영합니다">
       <Today/>
       <Developer/>
-      <Webd/>
-      <Website/>
-      <Portfolio/>
-      <Youtube/>
+      {/*데이터, 타이틀, id를 VideoSlider로 prop으로 전달 */}
+      <VideoSlider videos={webdText} title="😁 웹디자인기능사 준비는 이걸로!" id="webd" />
+      <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
+      <VideoSlider videos={portfolioText} title="🤗 포트폴리오 만드는 방법을 공유합니다." id="portfolio" />
+      <VideoSlider videos={youtubeText} title="😱 지금 이 코딩을 영상으로" id="youtube" />
     </Main>
   )
 }
